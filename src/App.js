6e8066cfb60dcb9e5e80e8295d31f495e441872a
jmalbite <albite.jm@gmail.com>
2020-12-08 21:44:02 +0800

    testing class component and this.state and high order map

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from "react";
+import { Button } from "@material-ui/core";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>My App</h1>
-    </div>
-  );
+class App extends React.Component{
+  constructor (){
+  super();
+    this.state = {
+      employees: [
+        {
+          name: 'Jann Mcihael Albite',
+          id: '16-0705'
+        },
+        {
+          name: 'Regine Eunice Raganas',
+          id: '17-0595'
+        }
+      ]
+    }  
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <h1>My App</h1>
+        {this.state.employees.map(employee => (
+          <h2 key={employee.id}> {employee.name} </h2>
+            ))}
+      </div>
+    );
+  }
 }
+
+
 
 export default App;
